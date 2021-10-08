5788e1b20ba81647947ada7104d6c316aacf63ad
pmpurcell93 <maddenpurcell@gmail.com>
2021-10-07 19:43:22 -0500

    Modified joke function

--- src/scripts/domEvents.js
+++ src/scripts/domEvents.js
@@ -2,18 +2,22 @@ import getJoke from './helpers/jokeData';
 import renderJokes from './renderJoke';
 import renderPunchline from './renderPunchline';
 
-const domEvents = (joke) => {
+const domEvents = () => {
+  let joke = {};
+  const setJoke = (obj) => {
+      joke = {
+          setup: obj.setup,
+          delivery: obj.delivery
+      };
+  }
   document.querySelector('#app').addEventListener('click', ((e) => {
-    if (e.target.id.includes('jokeButton')) {
-      console.warn(joke);
+    if (e.target.id.includes('jokeButton' || 'newJoke')) {
+      getJoke().then(setJoke);
       renderJokes(joke.setup);
     }
     if (e.target.id.includes('Punchline')) {
       console.warn('Clicked punchline button');
       renderPunchline(joke.delivery);
-    }
-    if (e.target.id.includes('newJoke')) {
-      getJoke().then((newJoke) => renderJokes(newJoke.setup));
     }
   }));
 };
