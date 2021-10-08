81c7b371df8a542ffb622a2708591e71c94c2e7d
pmpurcell93 <maddenpurcell@gmail.com>
2021-10-07 21:25:08 -0500

    Added basic styling

--- src/scripts/renderJoke.js
+++ src/scripts/renderJoke.js
@@ -3,8 +3,11 @@ import clearDom from './clearDom';
 const renderJokes = (obj) => {
   clearDom();
   document.querySelector('#app').innerHTML = `
+    <div id="jokebox">
+    <h3>Welcome to the Jokebox!</h3>
     <h4>${obj.setup}</h4>
-    <button type="button" id="Punchline" class="btn btn-primary">Punchline</button>
+    <button type="button" id="Punchline" class="btn btn-primary joke-button">Punchline</button>
+    </div>
     `;
 };
 
